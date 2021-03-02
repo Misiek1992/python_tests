2a6a42cfe4e419c0c136b951c887ad52bceccf35
Michał Ignaczak <m.ignaczak.92@gmail.com>
2021-03-02 15:12:02 +0100

    Update googlemaps_example.py

--- googlemaps_example.py
+++ googlemaps_example.py
@@ -4,7 +4,7 @@ from datetime import datetime
 # You have to enable API uasge https://console.cloud.google.com/apis/library?project=cedar-oath-273816
 
 if __name__ == '__main__':
-    gmaps = googlemaps.Client(key='AIzaSyCLVTBtdcceVqe-JkM83xvl1y5yOtBhIWw')
+    gmaps = googlemaps.Client(key='xxxxxx')
 
     # Geocoding an address
     geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
